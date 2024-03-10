b11b5285becb5ade0aa0f00485402c94656a1400
Hubdar Ali Rind <159878422+rind-hubdar@users.noreply.github.com>
2024-03-10 13:00:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// Arrays => 
+//string type
+var brothers = ["Ali", "Hubdar", "Zubair"];
+console.log(brothers);
+//Print a name one by one 
+for (var i = 0; i <= brothers.length; i++) {
+    console.log(brothers[i]);
+}
+//number type
+var evenNumbers = [2, 4, 6, 8, 10];
+var smallest_even_number = evenNumbers[0];
+console.log(smallest_even_number);
+console.log(evenNumbers);
+//boolean type
+var totalSum = [true, false, true, true];
+var totalSumOfSecondEqution = totalSum[3];
+console.log(totalSumOfSecondEqution);
+console.log(totalSum);
+//varible cases
+//CAMEL CASE  
+var myName = "Hubdar Ali Rind";
+//SNAKE CASE
+var my_age_is = 17;
+var real_age = my_age_is + 2;
+//PASCAL CASE
+var TotalPrice = 148;
+//THAT'S END I HOPE YOU'LL APPRICIATE IT. 
---
+++ main.ts
@@ -0,0 +1,48 @@
+// Arrays => 
+
+//string type
+let brothers: string[] = ["Ali","Hubdar","Zubair"];
+console.log(brothers)
+
+//Print a name one by one 
+for(let i = 0; i <= brothers.length; i++){
+    console.log(brothers[i])
+}
+
+
+//number type
+
+let evenNumbers: number[] = [2,4,6,8,10];
+
+let smallest_even_number = evenNumbers[0];
+
+console.log(smallest_even_number)
+console.log(evenNumbers);
+
+//boolean type
+
+let totalSum: boolean[] = [true,false,true,true];
+
+let totalSumOfSecondEqution = totalSum[3];
+
+console.log(totalSumOfSecondEqution);
+console.log(totalSum);
+
+
+
+//varible cases
+
+//CAMEL CASE  
+let myName: string = "Hubdar Ali Rind"
+
+//SNAKE CASE
+
+let my_age_is: number = 17;
+let real_age: number = my_age_is + 2;
+
+
+//PASCAL CASE
+
+let TotalPrice: number = 148;
+
+//THAT'S END I HOPE YOU'LL APPRICIATE IT. 
